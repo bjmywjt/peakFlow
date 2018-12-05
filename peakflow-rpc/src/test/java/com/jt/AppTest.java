814be5a1c3f7985b7c4a8ef50d98121625e75829
bjmywjt <bjmywjt@126.com>
2018-12-05 10:35:02 +0800

    tj

--- peakflow-rpc/src/test/java/com/jt/AppTest.java
+++ peakflow-rpc/src/test/java/com/jt/AppTest.java
@@ -2,6 +2,7 @@ package com.jt;
 
 import static org.junit.Assert.assertTrue;
 
+import com.google.common.collect.Maps;
 import com.jt.service.SnakerEngineFacets;
 import com.jt.utils.Profiles;
 import org.junit.Test;
@@ -9,13 +10,16 @@ import org.snaker.engine.SnakerEngine;
 import org.snaker.engine.access.QueryFilter;
 import org.snaker.engine.entity.HistoryTask;
 import org.snaker.engine.entity.Process;
+import org.snaker.engine.helper.StreamHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Unit test for simple App.
@@ -53,5 +57,16 @@ public class AppTest extends AbstractTransactionalJUnit4SpringContextTests{
         tasks.stream().forEach(historyTask -> {
             System.out.println(historyTask.toString());
         });
+    }
+
+    @Test
+    @Rollback(false)
+    public void testAll(){
+//        engine.process().deploy(StreamHelper.getStreamFromClasspath(""));
+//        engine.process().undeploy("03594189e5f04c5f923ba276d35a1185");
+        Map<String, Object> paraMap = Maps.newHashMap();
+        paraMap.put("desc","快速整改");
+        paraMap.put("deadline", 30);
+        engine.startInstanceById("f378448e4adf4ccd99688bbc659281b4","wangjiangtao",paraMap);
     }
 }
